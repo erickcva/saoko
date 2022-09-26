019b5655dd1fc0bd2690340f060835be6014885a
Erick Cepeda <ecepedavelazquez@gmail.com>
2022-09-26 10:55:50 -0500

    feat: separar script a carpeta

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko")
+document.getElementById("Generation").style.color ="grey";
+function colorTitulo(){
+  console.log("Pulso el boton de generation");
+  document.getElementById("Generation").style.color ="blue";
+}
